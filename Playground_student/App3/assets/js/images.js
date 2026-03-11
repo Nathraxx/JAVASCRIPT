5029a72c52a7f14c3d499f4195d5b53aa07be117
Tomas Runnvik <tom_89@live.se>
2026-03-11 11:31:30 +0100

    saas

--- Playground_student/App3/assets/js/images.js
+++ Playground_student/App3/assets/js/images.js
@@ -6,3 +6,12 @@ for (let index = 0; index < 10; index++) {
     console.log(index);
 }
 
+const imageList = document.querySelector('#imageList');
+
+const imageItem = document.createElement("div");
+imageItem.classList.add("imgItem");
+
+
+const p = document.createElement("p");
+imageItem.appendChild(p);
+imageList.appendChild(imageItem);
