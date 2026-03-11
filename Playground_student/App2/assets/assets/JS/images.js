c9ffe2407fad8f49c85d06b45dd0312de7497373
Tomas Runnvik <tom_89@live.se>
2026-03-11 10:23:06 +0100

    wat

---
+++ Playground_student/App2/assets/assets/JS/images.js
@@ -0,0 +1,8 @@
+'use strict';
+import {seedGenerator, uniqueId, randomNumber, deepCopy} from '../../../../../SeidoHelpers/seido-helpers.js';
+
+console.log("Hello world");
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+}
+
--- Playground_student/App2/assets/css/JS/images.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-import {seedGenerator, uniqueId, randomNumber, deepCopy} from `../../../../../SeidoHelpers/seido-helpers-test.js`;
-
-console.log("Hello world");
-for (let index = 0; index < 10; index++) {
-    console.log(index);
-}
